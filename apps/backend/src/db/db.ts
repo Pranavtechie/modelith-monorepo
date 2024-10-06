f20ec1bbf292bcf07a9e6b0df2e5343fc51c0c75
Pranav <pranav.techiegeek@gmail.com>
2024-10-07 04:02:23 +0530

    ready

--- apps/backend/src/db/db.ts
+++ apps/backend/src/db/db.ts
@@ -2,15 +2,23 @@ import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 import { DB } from './types'
 import fs from "fs"
+import env from "@repo/env"
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: fs.readFileSync('/etc/ssl/certs/global-bundle.pem').toString(),
-  },
 
-})
+let dbConnectionObject = {
+}
+
+if (env.NODE_ENV === 'production') {
+  dbConnectionObject = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: true,
+      ca: fs.readFileSync('/etc/ssl/certs/global-bundle.pem').toString(),
+    },
+  }
+}
+
+const pool = new Pool(dbConnectionObject)
 
 export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
