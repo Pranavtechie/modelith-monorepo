b0046c76ede6d6f899e6e3569ba2fad68b16faaf
Pranav <pranav.techiegeek@gmail.com>
2024-10-07 03:56:20 +0530

    ready

--- apps/backend/src/db/db.ts
+++ apps/backend/src/db/db.ts
@@ -1,9 +1,15 @@
 import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 import { DB } from './types'
+import fs from "fs"
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync('/etc/ssl/certs/global-bundle.pem').toString(),
+  },
+
 })
 
 export const db = new Kysely<DB>({
