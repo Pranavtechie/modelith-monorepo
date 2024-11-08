cf7013b939f85be14d9e15781ad3aff891ec47a6
Pranav <pranav.techiegeek@gmail.com>
2024-11-08 20:26:47 +0530

    ready

--- apps/backend/src/db/db.ts
+++ apps/backend/src/db/db.ts
@@ -2,24 +2,26 @@ import { Pool } from 'pg'
 import { Kysely, PostgresDialect } from 'kysely'
 import { DB } from './types'
 
-const isProduction = process.env.NODE_ENV === 'production' ? true : false;
+// const isProduction = process.env.NODE_ENV === 'production' ? true : false;
 
 let connectionObject = {
+  connectionString: process.env.DATABASE_URL,
 }
 
-if (isProduction) {
-  connectionObject = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  }
-} else {
-  connectionObject = {
-    connectionString: process.env.DATABASE_URL
+// if (isProduction) {
+//   connectionObject = {
 
-  }
-}
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   }
+// } else {
+//   connectionObject = {
+//     connectionString: process.env.DATABASE_URL
+
+//   }
+// }
+
 const pool = new Pool(connectionObject)
 
 
